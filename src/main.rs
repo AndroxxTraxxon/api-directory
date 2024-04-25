8c8a5db6558053bf0d27433bf9ce07426d70e6c0
David Culbreth <david.culbreth.256@gmail.com>
2024-04-25 11:52:13 -0500

    changing log level

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ mod users;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug")).init();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     let socket_addr = "127.0.2.1:443";
     let tls_config = secconf::load_tls_config()?;
     let jwt_config = web::Data::new(secconf::load_jwt_config()?);
